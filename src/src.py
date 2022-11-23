6dca0a68d72cd0eb4dcfc37ee70a4ac9f0ff64c4
Mikhail <46998030+Khail909@users.noreply.github.com>
2022-11-23 21:19:19 +0300

    added colour tables

---
+++ src/src.py
@@ -0,0 +1,19 @@
+import pygame
+import time
+import random
+ 
+pygame.init()
+
+font_style = pygame.font.SysFont("bahnschrift", 25)
+score_font = pygame.font.SysFont("comicsansms", 35)
+ 
+BLACK = (0, 0, 0)
+GRAY = (127, 127, 127)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+CYAN = (0, 255, 255)
+MAGENTA = (255, 0, 255)
+ORANGE = (255,100,10)
