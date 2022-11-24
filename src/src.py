e57ebf45e616d863e87c84ded913da0ab251289b
Mikhail <46998030+Khail909@users.noreply.github.com>
2022-11-24 16:20:59 +0300

    game loop updated: the snake can now move

--- src/src.py
+++ src/src.py
@@ -1,4 +1,6 @@
 import pygame
+import time
+import random
  
 pygame.init()
  
@@ -13,45 +15,80 @@ CYAN = (0, 255, 255)
 MAGENTA = (255, 0, 255)
 ORANGE = (255,100,10)
  
-dis = pygame.display.set_mode((800, 600))
+dis = pygame.display.set_mode((1440, 1080))
 pygame.display.set_caption('Dream team')
- 
-game_over = False
- 
-x = 150
-y = 150
- 
-x_change = 0       
-y_change = 0
  
 clock = pygame.time.Clock()
  
-while not game_over:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            game_over = True
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                y_change = -5
-                x_change = 0
-            elif event.key == pygame.K_DOWN:
-                y_change = 5
-                x_change = 0
-            elif event.key == pygame.K_LEFT:
-                x_change = -5
-                y_change = 0
-            elif event.key == pygame.K_RIGHT:
-                x_change = 5
-                y_change = 0
+snake = 20
+snake_speed = 15
  
-    x += x_change
-    y += y_change
-    dis.fill(GREEN)
-    pygame.draw.rect(dis, ORANGE, [x, y, 5, 5])
+font_style = pygame.font.SysFont(None, 30)
  
-    pygame.display.update()
  
-    clock.tick(20)
+def message(msg, color):
+    mesg = font_style.render(msg, True, color)
+    dis.blit(mesg, [1440/3, 1080/3])
  
-pygame.quit()
-quit()
+ 
+def gameLoop():  # creating a function
+    close = False
+    over = False
+ 
+    x = 1440 / 2
+    y = 1080 / 2
+ 
+    foodx = round(random.randrange(0, 1440 - snake) / 10.0) * 10.0
+    foody = round(random.randrange(0, 1080 - snake) / 10.0) * 10.0
+    xc = 0
+    yc = 0
+ 
+    while not over:
+ 
+        while close == True:
+            dis.fill(GREEN)
+            message("Game over! 'r' - restart, q - 'quit'", BLACK)
+            pygame.display.update()
+ 
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q or event.key == pygame.K_й:
+                        over = True
+                        close = False
+                    if event.key == pygame.K_r or event.key == pygame.K_к:
+                        gameLoop()
+ 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                over = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    yc = -snake
+                    xc = 0
+                elif event.key == pygame.K_DOWN:
+                    yc = snake
+                    xc = 0
+                elif event.key == pygame.K_LEFT:
+                    xc = -snake
+                    yc = 0
+                elif event.key == pygame.K_RIGHT:
+                    xc = snake
+                    yc = 0
+        x+=xc
+        y+=yc
+ 
+        if x >= 1440 or x < 0 or y >= 1080 or y < 0:
+            close = True
+
+        dis.fill(GREEN)
+        pygame.draw.rect(dis, RED, [foodx, foody, snake, snake])
+        pygame.draw.rect(dis, YELLOW, [x, y, snake, snake])
+        pygame.display.update()
+ 
+        clock.tick(snake_speed)
+ 
+    pygame.quit()
+    quit()
+ 
+ 
+gameLoop()
